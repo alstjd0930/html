2ab66f66f9f114f787ac30684d14dec37bbf4b8d
alstjd0930 <alstjd0930@naver.com>
2023-06-27 17:51:09 +0900

    0627

---
+++ 0626_js/test.js
@@ -0,0 +1,12 @@
+//script 태그 작성하지않음
+//팝업창 종류
+//1. alert - 확인
+alert("이건alert")
+//2. comfirm - 확인/취소(true/false 값이 return)
+var confirmvalue = confirm("컨펌창");
+alert(confirmvalue);
+console.log(confirmvalue);
+//3. prompt - 입력 - 확인/취소(입력한 값이 return됨)
+var promptvalue = prompt("프롬")
+alert(promptvalue);
+console.log(promptvalue);
